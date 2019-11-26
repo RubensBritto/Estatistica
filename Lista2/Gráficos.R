00855cc1bddbc730bae5906da1f09421c747fb00
lucasalisboa <43450181+lucasalisboa@users.noreply.github.com>
2019-11-26 14:13:59 -0300

    Create Gráficos.R

---
+++ Lista2/Gráficos.R
@@ -0,0 +1,6 @@
+#Questão 5
+curve(dnorm(x,mean = 850,sd = 40),850 - 3*40,850 + 3*40,xlab = "Dias", ylab = "Probabilidade", main = "Duração dos Componentes") 
+#Questão 8
+curve(dnorm(x,mean = 4000,sd = 200),4000 - 3*200,4000 + 3*200,xlab = "Cartas", ylab = "Probabilidade", main = "Volume de Correspondência")
+#Questão 9
+curve(dnorm(x,mean = 1000,sd = 100),1000 - 3*100,1000 + 3*100,xlab = "Dias", ylab = "Probabilidade", main = "Vida Útil das Lâmpadas")
